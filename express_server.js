75b78e7d4fdc1902e84df69a0084d9c2b3b7a446
angelareit <anreit01@email.com>
2023-03-21 04:01:06 +0000

    functional form submission and redirection using shortened urls

--- express_server.js
+++ express_server.js
@@ -43,10 +43,20 @@ app.get("/urls/:id", (req, res) => {
   res.render("urls_show", templateVars);
 }); 
 
+app.get("/u/:id", (req, res) => {
+  const longURL = urlDatabase[req.params.id];
+  console.log(longURL);
+  res.redirect(longURL);
+});
+
 app.post("/urls", (req, res) => {
   console.log(req.body); // Log the POST request body to the console
-  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+  let urlId = generateRandomString();
+  urlDatabase[urlId] = req.body.longURL;
+  //res.send("Ok"); // Respond with 'Ok' (we will replace this)
+  res.redirect(`urls/:${urlId}`);
 });
+
 
 function generateRandomString() {
   let randomStr = Math.random().toString(36).slice(2, 8);
